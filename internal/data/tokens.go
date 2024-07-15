c19a79957f72ea29e763e0c3f650bb1d5fd202f7
GustavoCesarSantos <gustavocs789@gmail.com>
2024-07-14 23:12:00 -0300

    atualiza projeto

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -12,8 +12,9 @@ import (
 )
 
 const (
-	ScopeActivation     = "activation"
-	ScopeAuthentication = "authentication"
+    ScopeActivation = "activation"
+    ScopeAuthentication = "authentication"
+    ScopePasswordReset = "password-reset"
 )
 
 type Token struct {
